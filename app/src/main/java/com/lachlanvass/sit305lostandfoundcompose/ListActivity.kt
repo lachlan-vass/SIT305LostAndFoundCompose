98829a6104c52f8130167384620abc7463c06983
lachlan-vass <lachlan.vass@superretailgroup.com>
2022-04-19 14:34:46 +1000

    card logic for posts

--- app/src/main/java/com/lachlanvass/sit305lostandfoundcompose/ListActivity.kt
+++ app/src/main/java/com/lachlanvass/sit305lostandfoundcompose/ListActivity.kt
@@ -3,15 +3,17 @@ package com.lachlanvass.sit305lostandfoundcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.lachlanvass.sit305lostandfoundcompose.ui.theme.SIT305LostAndFoundComposeTheme
 
 class ListActivity : ComponentActivity() {
@@ -20,28 +22,6 @@ class ListActivity : ComponentActivity() {
         val items = (1..50).toList()
 
         val db = AppDatabase.getDatabase(this)
-
-        val examplePost = Post(
-            1,
-            "hello",
-            "1234ABC",
-            "I lost my cat",
-            "1/21/11",
-            "Brisbane"
-        )
-
-        val examplePost2 = Post(
-            2,
-            "hello",
-            "1234ABC",
-            "I lost my cat",
-            "1/21/11",
-            "Brisbane"
-        )
-
-        db.postDao().insert(examplePost)
-        db.postDao().insert(examplePost2)
-
         val posts = db.postDao().getAll()
 
 
@@ -52,7 +32,15 @@ class ListActivity : ComponentActivity() {
                 posts.forEach {
                         post -> Row() {
 
-                    Text(text = post.toString())
+                    Card(
+                        shape = MaterialTheme.shapes.medium,
+                        backgroundColor = MaterialTheme.colors.surface,
+                        contentColor = contentColorFor(backgroundColor = MaterialTheme.colors.surface),
+                        elevation = 10.dp,
+                        modifier = Modifier.padding(10.dp)
+                    ) {
+                        Text(text = "${post.name} Posted on: ${post.date}" ?: "New Post" , modifier = Modifier.padding(10.dp))
+                    }
 
                 }
                 }
